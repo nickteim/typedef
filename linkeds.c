06d0f6b9fe9395aa86ba7a97e6090c998760785f
nickteim <44054399+nickteim@users.noreply.github.com>
2019-09-21 22:39:17 +0200

    Create linkeds.c

---
+++ linkeds.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#ifndef bool            //make bool work in c, since it is not standard
+    #define bool int
+    #define false ((bool)0)
+    #define true  ((bool)1)
+#endif
+
+
+
+struct node
+{
+    int number;
+    struct node *next;            //EACH pointer point to another node called next
+};
+typedef struct node node;
+
+
+
+
+
+int main( void )
+{
+    //MOMORY for numbers
+    node*numbers=NULL;             //THE begining of the list
+
+
+    //PROMT USER FOR INPUT TILL EOF
+    while(&free)                    //while true
+    {
+        //promt for number
+
+        int number;
+        printf( "number:" );
+        scanf( "%i", &number );
+
+
+        //check EOF
+        if (number==42)
+        {
+            break;
+        }
+
+
+        //check if number already is in the list
+        bool found=false;
+        for (node*ptr=numbers;ptr!=NULL;ptr=ptr->next)        //ptr is a pointer that looks over every number in the list; like litterally the same as if you took your and and pointed on ech number
+        {                                                     //ptr=ptr->next means update whatever the number ptr is pointing at to what that number is pointing at. in other words ptr can go though the whole list
+            if(ptr->number==number)
+            {
+                found =true;
+                break;
+            }
+
+        }
+
+
+        // if number is not in the list add it to the list
+        node*n=malloc(sizeof(node));
+        if(!n)      //if there is not enouht space stop program
+        {
+            return 1;
+        }
+
+        //add number to list
+        n->number=number;
+        n->next=NULL;
+        if(numbers)
+        {
+            for (node*ptr=numbers;ptr!=NULL;ptr=ptr->next)
+             {
+                 if(!ptr->next)             //!ptr->next means ptr->==NULL, means if found the end of the list go and update whatever ptr is pointing at with n
+                {
+                    ptr->next=n;
+                    break;
+                }
+
+             }
+        }
+        else
+        {
+            numbers=n;
+        }
+
+
+
+
+}
+  //print numbers in list
+       for (node*ptr=numbers;ptr!=NULL;ptr=ptr->next)
+        {
+            printf("%i\n", ptr->number);
+        }
+
+
+        //free momory
+        node*ptr=numbers;
+        while(ptr!=NULL)
+        {
+            node*next=ptr->next;
+            free(ptr);
+            ptr=next;
+        }
+
+}
+
+
