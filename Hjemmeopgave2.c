02c7c9c0759fd41531e9631f112a15e7c3fcd71c
nickteim <44054399+nickteim@users.noreply.github.com>
2019-09-24 15:30:06 +0200

    Create Hjemmeopgave2.c

---
+++ Hjemmeopgave2.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+
+int TemperatureConv (int Kelvin,int Celsius)
+{
+Celsius = 5.0 / 9.0 * (Farenheit - 32.0);
+Kelvin = Celsius + 273.16; 
+return TemperatureConv;
+ }  
+
+int main(void)
+{
+    int Farenheit;
+    
+    while (scanf("%i",&Farenheit)==2)
+    {
+        printf("%i,%i,%i",Farenheit,Celsius,Kelvin);
+    }
+    return 0;
+}
